5c66dde2e8c174fa010e1ae430a824916668439b
Christian Villarosa <cvillarosa25@gmail.com>
2026-05-05 07:42:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6549033679-5ca07",
+  "appId": "1:405229465701:web:1f2cebc2afdd5a0654968c",
+  "apiKey": "AIzaSyArstQGkhiQbLik3xRj7BV2MTOHc3X3HZ0",
+  "authDomain": "studio-6549033679-5ca07.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "405229465701"
+};
