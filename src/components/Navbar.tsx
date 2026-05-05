871feb6da3fbf91db59b8685b7169800be69dce5
Christian Villarosa <cvillarosa25@gmail.com>
2026-05-05 09:08:41 +0000

    Call Stack 4 Show 1 ignore-listed frame(s) FirestorePermissionError src/

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export function Navbar() {
 
   return (
     <nav className="bg-white border-b sticky top-0 z-50 no-print">
-      <div className="max-w-7xl auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex items-center gap-2">
             <Link href="/" className="flex items-center gap-3 group">
